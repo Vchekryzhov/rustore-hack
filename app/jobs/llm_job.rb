62656b7dc576c7a431cf1fc0c5bc163d872e3f14
Viktor C <vchekryzhov@ya.ru>
2024-07-06 17:06:18 +0200

    master: Fix llm

--- app/jobs/llm_job.rb
+++ app/jobs/llm_job.rb
@@ -8,12 +8,18 @@ class LlmJob < ApplicationJob
     config = Config.first_or_create
     system_prompt = config.system
     user_prompt = config.user % { context:, query: message.text }
-    llm_response = Llm.call(system_prompt: , user_prompt: )
-    text = if config.llm_enabled && llm_response[:success]
-             llm_response[:message]
+    text = if config.llm_enabled
+             llm_response = Llm.call(system_prompt: , user_prompt: )
+             if llm_response[:success]
+                      llm_response[:message]
+                    else
+                      build_fallback_response(articles)
+                    end
            else
              build_fallback_response(articles)
            end
+
+
     Message.create(text: , room_id: message.room_id, user: User.find_or_create_by(name: "admin"))
   end
   def build_context(articles)
